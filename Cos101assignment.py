d1f7afc078df86501e51fda551e4bfb5a401c365
og-ushie1 <kingdavidogar123@gmail.com>
2024-11-17 15:23:42 -0800

    Add files via upload

---
+++ Cos101assignment.py
@@ -0,0 +1,35 @@
+print("formular for distance ")
+# formula for distace(d)=speed*time
+s=float(input("enter the value for speed: "))
+t=float(input(" enter the value for time: "))
+print(s*t)
+
+
+
+print("formular for finding the volume ")
+# formula for volume= length(l) * width(w) * height(h)
+l=float(input("enter the value for length: "))
+w=float(input(" enter the value for width: "))
+h=float(input(" enter the value for height: "))
+print(l*w*h)
+
+
+
+print("formular for area of rectangle ")
+# formula for rectangle = lenght(l) * width(w)
+l=float(input("enter the value for length: "))
+w=float(input(" enter the value for width: "))
+print(l*w)
+
+
+print("formular for  perimeter of a rectangle")
+# formula for perimeter of a rectangle = 2l+2w
+l=float(input("enter the value for length: "))
+w=float(input(" enter the value for width: "))
+print(2*l+2*w)
+
+
+print("formular for converting kelvin to celsius")
+# formula for converting kelvin to celsius = +270
+k=float(input("enter the value for kelvin: "))
+print(k+270)
